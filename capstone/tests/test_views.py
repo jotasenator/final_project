4bbeeee168cead57228951978fc69de57d261b09
Maykel <jotasenator@gmail.com>
2023-07-03 19:52:10 -0400

    Test views submit_issue function

--- capstone/tests/test_views.py
+++ capstone/tests/test_views.py
@@ -1,31 +1,26 @@
-# python manage.py test capstone.tests.test_views
-from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
 from capstone.models import User, Issue
-from datetime import datetime, timedelta
+from datetime import timedelta
+from django.utils import timezone
 
 
-class SubmitIssueTest(TestCase):
+class SubmitIssueTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(
-            username="testuser", email="test@example.com", password="testpassword"
-        )
-        self.client.force_login(self.user)
+        self.user = User.objects.create_user(username="testuser", password="testpass")
+        self.client.login(username="testuser", password="testpass")
 
     def test_submit_issue(self):
         response = self.client.post(
-            "/submit_issue/", {"issue": "Test Issue", "description": "Test Description"}
+            "/submit_issue", {"issue": "Test Issue", "description": "Test Description"}
         )
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, "/")
-
-        # Test if the issue was created successfully
-        issue = Issue.objects.get(issue="Test Issue", description="Test Description")
-        self.assertEqual(issue.issue, "Test Issue")
+        self.assertRedirects(response, "/")
+        issue = Issue.objects.get(issue="Test Issue")
         self.assertEqual(issue.description, "Test Description")
         self.assertEqual(issue.user, self.user)
 
         # Test if the created_at field was set correctly
-        now = datetime.now()
+        now = timezone.now()
         self.assertLessEqual(issue.created_at, now)
         self.assertGreaterEqual(issue.created_at, now - timedelta(seconds=1))
